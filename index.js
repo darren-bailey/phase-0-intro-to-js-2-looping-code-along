c361ed22352531957490949c619d1732c09ec3af
Darren Bailey <dbailey473@yahoo.com>
2022-12-08 13:11:17 -0800

    commit tree 71125207411e72a5ad0d5cc0ad55d18be815040b

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solutions in this file
+const names = [...(3)];
+function writeCards(names) {
+    for (let i = 0; i < names.length; i++) {
+        console.log(`Thank you, ${names[i]} for the wonderful surprise gift!`);
+    }
+    return names;
+}
+writeCards(names);
